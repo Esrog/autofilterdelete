861adab6233a4a3fad42bbf806c1158b62d8c3d8
Logesh <116418351+LPSMOVIES@users.noreply.github.com>
2022-11-30 08:21:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,8 +35,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Bilal8831:<bilal8831>@himawari.vmy7rdj.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "himawari")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Db:db@padangaldb.r5zuxzj.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Padangaldb")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
